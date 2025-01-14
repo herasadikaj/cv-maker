2a2ba956fb27b7e7c192cae6ac6aec675387b254
Hera Sadikaj <hera.sadikaj@yahoo.com>
2025-01-14 15:07:05 +0100

    new

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,27 +1,31 @@
 import { useNavigate } from 'react-router-dom';
 import './pages.css';
+
 const Homepage = () => {
+  const navigate = useNavigate();
 
-const navigate=useNavigate();
+  const handleStart = () => {
+    navigate('/personal');
+  };
 
-const handleStart=()=>{
-    navigate('/personal')
-}
-const handleList=()=>{
+  const handleList = () => {
     navigate('/list');
-}
-return (
+  };
 
+  return (
     <div className="homepage">
-    <h1> WELCOME</h1>
-    <h4>BUILD YOUR CV WITH US</h4>
-
-    <div className="butona-welcome">
-     <button className="check" onClick={handleList}>Check CV</button>
-      <button className="create" onClick={handleStart}>Create CV</button>
-     </div>
+      <h1>WELCOME</h1>
+      <h4>BUILD YOUR CV WITH US</h4>
+      <div className="butona-welcome">
+        <button className="create" onClick={handleStart}>
+          Start Creating
+        </button>
+        <button className="check" onClick={handleList}>
+          Check CVs
+        </button>
+      </div>
     </div>
-)
+  );
+};
 
-}
 export default Homepage;
